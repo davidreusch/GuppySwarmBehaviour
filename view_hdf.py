c28737785796f333ef5c540c670a7d777532465a
David Reuschenberg <davidreuschenberg@web.de>
2020-05-30 18:33:07 +0200

    Calculation of locomotion vector added, simulation corrected and finished

--- view_hdf.py
+++ view_hdf.py
@@ -10,15 +10,24 @@ from descartes.patch import PolygonPatch
 from figures import BLUE, RED, BLACK, YELLOW, SIZE, set_limits, plot_coords, color_isvalid
 
 
+# Whole code is inspired by Moritz Maxeiners master thesis and his code for the thesis
+
+
+# some simple helper functions
+
 # convert a unit-vector giving the orientation to its angle to the x-axis (in radians)
 def vec_to_angle(x, y):
     if y >= 0:
         return acos(x)
     else:
-        return 2 * pi - acos(x)
+
+        return -acos(x)
 
 
-# map distance to value in range(0,1) - greater distance -> lesser intensity
+max_dist = 70
+
+
+# map distance to value in range(0,1) - greater distance -> lesser intensity (from Moritz Maxeiner)
 def intensity_linear(dist, max_dist):
     if dist < 0 or max_dist < dist:
         return 0
@@ -38,165 +47,189 @@ def addCorner(p1, p2):
     return sympy.Point2D(x, y)
 
 
-def plot_wall_rays(tank, obs_pos: sympy.Point2D, intersections):
-    #    fig = pyplot.figure(1, figsize=SIZE, dpi=90)
-    #    ax = fig.add_subplot(121)
-
-    # set up axes
-    fig, ax = pyplot.subplots()
-    set_limits(ax, 0, 100, 0, 100)
-    ax.set_title('tank')
-
-    # plot tank
-    ext = [(0, 0), (0, 100), (100, 100), (100, 0)]
-    # int = [(1, 0), (0.5, 0.5), (1, 1), (1.5, 0.5), (1, 0)][::-1]
-    polygon = ShapelyPolygon(ext)
-    # plot_coords(ax, polygon.interiors[0])
-    plot_coords(ax, polygon.exterior)
-    patch = PolygonPatch(polygon, facecolor=BLUE, edgecolor=BLACK, alpha=0.5, zorder=1)
-    ax.add_patch(patch)
-
-    # plot rays
-    ax.plot(obs_pos.x, obs_pos.y, "ro")
-    for (x, y) in intersections:
-        ax.plot([obs_pos.x, x], [obs_pos.y, y])
-
-    pyplot.show()
+def dot_product(vec1, vec2):
+    akk = 0
+    for i in range(len(vec1)):
+        akk += vec1[i] * vec2[i]
+    return akk
 
 
-def plot_guppy_bins(tank, obs_pos: sympy.Point2D, bins, bin_intensities, others, intersections):
-    #    fig = pyplot.figure(1, figsize=SIZE, dpi=90)
-    #    ax = fig.add_subplot(121)
-
-    # set up axes
-    fig, ax = pyplot.subplots()
-    ax.set_title('tank')
-    set_limits(ax, 0, 100, 0, 100)
-
-    # plot tank
-    ext = [(0, 0), (0, 100), (100, 100), (100, 0)]
-    polygon = shapely.geometry.Polygon(ext)
-    plot_coords(ax, polygon.exterior)
-    # int = [(1, 0), (0.5, 0.5), (1, 1), (1.5, 0.5), (1, 0)][::-1]
-    # plot_coords(ax, polygon.interiors[0])
-    patch = PolygonPatch(polygon, facecolor=BLUE, edgecolor=BLACK, alpha=0.6, zorder=1)
-    ax.add_patch(patch)
-
-    # plot bins
-    for i, p in enumerate(bins):
-        patch = PolygonPatch(sympyToShapelyPolygon(p), facecolor=BLACK, edgecolor=YELLOW,
-                             alpha=(1 - bin_intensities[i]), zorder=1)
-        ax.add_patch(patch)
-
-    # plot fishes
-    ax.plot(obs_pos.x, obs_pos.y, "go")
-    for i in range(len(others)):
-        ax.plot([others[i].x], [others[i].y], "r.")
-
-    pyplot.show()
+def get_locomotion_vec(data_prev, data_cur):
+    if data_prev is None:
+        return 0, 0
+    else:
+        ang_turn = vec_to_angle(data_prev[2], data_prev[3]) - vec_to_angle(data_cur[2], data_cur[3])
+        diff_vec = ((data_cur[0] - data_prev[0]), (data_cur[1] - data_prev[1]))
+        linear_speed = dot_product(diff_vec, (data_cur[2], data_cur[3]))
+        return ang_turn, linear_speed
 
 
-max_dist = 70
+def dist_travelled(data_prev, data_cur):
+    if data_prev is None:
+        return 0, 0
+    else:
+        return sqrt((data_cur[0] - data_prev[0]) ** 2 + (data_cur[1] - data_prev[1]) ** 2)
 
 
-def wall_distances(tank: Polygon, obs_pos: sympy.Point2D, num_wall_angles: int):
-    def wall_distance(angle):
-        # find distance to wall along a given angle
-        ray = sympy.Ray(obs_pos, angle=angle)
-        intersection = ray.intersection(tank)
-        return obs_pos.distance(intersection[0]), (intersection[0].x, intersection[0].y)
+# thats the main class, its run method will do all the work
+class Guppy_Calculator():
+    def __init__(self, hdf_file, agent, num_bins, num_rays):
+        # set up data
+        self.ls = list(f.keys())
+        self.data = [hdf_file.get("{}".format(i)) for i in range(len(self.ls))]
+        self.agent_data = numpy.array(self.data[agent])
 
-    # find distances for all angles
-    angles = [pi * (i / num_wall_angles) + obs_angle for i in range(0, num_wall_angles + 1)]
-    distances = []
-    intersections = []
-    for a in angles:
-        d, i = wall_distance(a)
-        # map distances to intensitys
-        distances.append(intensity_linear(d, max_dist))
-        intersections.append(i)
-    return distances, intersections
+        # set up axes
+        self.fig, self.ax = pyplot.subplots()
+        self.ax.set_title('tank')
+        set_limits(self.ax, 0, 100, 0, 100)
+        # tank coordinates are just 100 x 100 square
+        coords = [(0, 0), (100, 0), (100, 100), (0, 100)]
+        self.tank = sympy.geometry.polygon.Polygon(*coords)
 
+        # data to be calculated
+        self.num_bins = num_bins
+        self.num_rays = num_rays
+        self.agent_view = None
+        self.wall_view = None
+        self.obs_pos = None
+        self.obs_ori = None
+        self.obs_angle = None
+        self.others = None
+        self.loc_vec = None
 
-def guppy_distances(tank: Polygon, obs_pos: sympy.Point2D, obs_angle: float, num_bins: int, others: [sympy.Point2D]):
-    # get the boundaries of the bins by dividing 180 degree field of view by number of bins
-    # and make a ray for each resulting angle
-    rays = [sympy.Ray(obs_pos, angle=pi * x / num_bins + obs_angle) for x in range(0, num_bins + 1)]
+    def craft_vector(self, i):
+        """calculate the vector v = (locomotion, agent_view, wall_view) from the raw data"""
+        # get position of guppy 0 and convert it to sympy point
+        self.obs_pos = sympy.Point2D(self.agent_data[i][0], self.agent_data[i][1])
+        # get orientation vector
+        self.obs_ori = (self.agent_data[i][2], self.agent_data[i][3])
+        # calculate angle of guppy_orientation with respect to x_axis
+        self.obs_angle = vec_to_angle(self.obs_ori[0], self.obs_ori[1])
+        # calculate positions of other guppys
+        self.others = [sympy.Point2D(self.data[j][i][0], self.data[j][i][1]) for j in
+                       range(1, len(self.ls))]
+        # calculate intensity vector wall_view for distance to walls
+        self.wall_distances()
+        # calculate intensity vector agent_view for distance to nearby guppies
+        self.guppy_distances()
+        # loc_vec[0] = angular turn; loc_vec[1] = linear speed
+        self.loc_vec = get_locomotion_vec(self.agent_data[i - 1], self.agent_data[i])
 
-    # get the intersection points of the rays with the tank walls
-    intersections = [ray.intersection(tank)[0] for ray in rays]
+        return self.loc_vec, self.agent_view, self.wall_view
 
-    # model the bins as polygons with intersections points and observer position as vertices
-    bins = []
-    for i in range(len(intersections) - 1):
-        if intersections[i][0] == intersections[i + 1][0] or intersections[i][1] == intersections[i + 1][1]:
-            bins.append(Polygon(obs_pos, intersections[i], intersections[i + 1]))
-        # if the intersection points overlap a corner of the tank, we have to add that corner to the polygon
-        else:
-            corner = addCorner(intersections[i], intersections[i + 1])
-            bins.append(Polygon(obs_pos, intersections[i], corner, intersections[i + 1]))
+    def run_sim(self, step):
+        for frame in range(1, len(self.agent_data), step):
+            self.craft_vector(frame)
+            self.dist_difference = dist_travelled(self.agent_data[frame - 1], self.agent_data[frame])
+            #self.plot_guppy_bins()
+            self.plot_wall_rays() #use either of the two plot_functions, not both at once
 
-    # loop through the bins and find the closest guppy for each bin
-    bin_distances = [1000000 for i in range(len(bins))]
-    length = len(others)
-    others_c = others[:]
-    for i in range(len(bins)):
-        j = 0
-        while j < length:
-            if bins[i].encloses_point(others_c[j]) and obs_pos.distance(others_c[j]) < bin_distances[i]:
-                bin_distances[i] = float(obs_pos.distance(others_c[j]))
-                del (others_c[j])  # if guppy is already in a bin, delete it
-                length -= 1
-            else:
-                j += 1
+    def guppy_distances(self):
+        # get the boundaries of the bins by dividing 180 degree field of view by number of bins
+        # and make a ray for each resulting angle. To adjust the rays to our angle-system, we have to subtract pi / 2
+        rays = [sympy.Ray(self.obs_pos, angle=pi * x / self.num_bins + self.obs_angle - pi / 2) for x in
+                range(0, self.num_bins + 1)]
 
-    print("closest distance for each bin:", bin_distances)
+        # get the intersection points of the rays with the tank walls
+        intersections = [ray.intersection(self.tank)[0] for ray in rays]
 
-    # convert distances to intensities in range(0,1)
-    for i in range(len(bin_distances)):
-        bin_distances[i] = intensity_linear(bin_distances[i], max_dist)  # sqrt(100*100 + 100*100))
+        # construct the bins as polygons with intersection points and observer position as vertices
+        self.bins = []
+        for i in range(len(intersections) - 1):
+            if intersections[i][0] == intersections[i + 1][0] or intersections[i][1] == intersections[i + 1][1]:
+                self.bins.append(Polygon(self.obs_pos, intersections[i], intersections[i + 1]))
+            else:  # if the intersection points overlap a corner of the tank, we have to add that corner to the polygon
+                corner = addCorner(intersections[i], intersections[i + 1])
+                self.bins.append(Polygon(self.obs_pos, intersections[i], corner, intersections[i + 1]))
 
-    # plot the result
-    plot_guppy_bins(tank, obs_pos, bins, bin_distances, others, intersections)
+        # loop through the bins and find the closest guppy for each bin
+        self.agent_view = [1000000 for i in range(len(self.bins))]
+        length = len(self.others)
+        others_c = self.others[:]
+        for i in range(len(self.bins)):
+            j = 0
+            while j < length:
+                if self.bins[i].encloses_point(others_c[j]) and self.obs_pos.distance(others_c[j]) < self.agent_view[i]:
+                    self.agent_view[i] = float(self.obs_pos.distance(others_c[j]))
+                    del (others_c[j])  # if guppy is already in a bin, delete it
+                    length -= 1
+                else:
+                    j += 1
 
-    return bin_distances
+        print("closest distance for each bin:", self.agent_view)
+        # convert distances to intensities in range(0,1)
+        for i in range(len(self.agent_view)):
+            self.agent_view[i] = intensity_linear(self.agent_view[i], max_dist)
+        # agent_view vector is ready now
+
+    def plot_guppy_bins(self):
+        self.ax.cla()  # clear axes
+
+        # plot tank
+        ext = [(0, 0), (0, 100), (100, 100), (100, 0)]
+        polygon = shapely.geometry.Polygon(ext)
+        plot_coords(self.ax, polygon.exterior)
+        patch = PolygonPatch(polygon, facecolor=BLUE, edgecolor=BLACK, alpha=0.6, zorder=1)
+        self.ax.add_patch(patch)
+
+        # plot bins
+        for i, p in enumerate(self.bins):
+            patch = PolygonPatch(sympyToShapelyPolygon(p), facecolor=BLACK, edgecolor=YELLOW,
+                                 alpha=(1 - self.agent_view[i]), zorder=1)
+            self.ax.add_patch(patch)
+
+        # plot fishes
+        self.ax.plot(self.obs_pos.x, self.obs_pos.y, "go")  # self
+        for i in range(len(self.others)):
+            self.ax.plot([self.others[i].x], [self.others[i].y], "r.")  # others
+
+        # plot legend
+        self.ax.text(110, 100, 'angle: {:.2f}°'.format(degrees(self.obs_angle)))
+        self.ax.text(110, 90, 'o_vector: ({:.2f},{:.2f})'.format(self.obs_ori[0], self.obs_ori[1]))
+        self.ax.text(110, 80, 'angular turn: {:.10}'.format(self.loc_vec[0]))
+        self.ax.text(110, 70, 'linear speed: {:.10}'.format(self.loc_vec[1]))
+        self.ax.text(110, 60, 'dist travelled: {:.10}'.format(self.dist_difference))
+
+        pyplot.show(block=False)
+        pyplot.pause(0.00000000001)
+
+    def wall_distances(self):
+        def wall_distance(angle):
+            # find distance to wall along a given angle
+            ray = sympy.Ray(self.obs_pos, angle=angle)
+            intersection = ray.intersection(self.tank)
+            return self.obs_pos.distance(intersection[0]), (intersection[0].x, intersection[0].y)
+
+        # find distances for all angles
+        angles = [pi * (i / self.num_rays) + self.obs_angle - pi / 2 for i in range(0, self.num_rays + 1)]
+        self.wall_view = []
+        self.intersections = []
+        for a in angles:
+            d, i = wall_distance(a)
+            # map distances to intensities
+            self.wall_view.append(intensity_linear(d, max_dist))
+            self.intersections.append(i)
+
+    def plot_wall_rays(self):
+        self.ax.cla()
+        # plot tank
+        ext = [(0, 0), (0, 100), (100, 100), (100, 0)]
+        polygon = shapely.geometry.Polygon(ext)
+        plot_coords(self.ax, polygon.exterior)
+        patch = PolygonPatch(polygon, facecolor=BLUE, edgecolor=BLACK, alpha=0.6, zorder=1)
+        self.ax.add_patch(patch)
+
+        # plot rays
+        self.ax.plot(self.obs_pos.x, self.obs_pos.y, "ro")
+        for (x, y) in self.intersections:
+            self.ax.plot([self.obs_pos.x, x], [self.obs_pos.y, y])
+
+        pyplot.show(block=False)
+        pyplot.pause(0.000001)
 
 
 with h5py.File("guppy_data/couzin_torus/train/8_0000.hdf5", "r") as f:
-    ls = list(f.keys())
-    print("Datasets in the first file with 8 guppys: ", ls)
-    data = [f.get("{}".format(i)) for i in range(len(ls))]
-
-    # choose guppy null
-    guppy_zero_data = numpy.array(data[0])
-    print(guppy_zero_data[0])
-
-    # tank coordinates are just 100 x 100 square
-    coords = [(0, 0), (100, 0), (100, 100), (0, 100)]
-    tank = sympy.geometry.polygon.Polygon(*coords)
-
-    for testframe in range(0, 700, 70):
-        # get position of guppy 0 and convert it to sympy point
-        obs_pos = sympy.Point2D(guppy_zero_data[testframe][0], guppy_zero_data[testframe][1])
-
-        # get orientation vector
-        obs_ori = (guppy_zero_data[testframe][2], guppy_zero_data[testframe][3])
-
-        # calculate angle of guppy_orientation with respect to x_axis
-        obs_angle = vec_to_angle(obs_ori[0], obs_ori[1])
-
-        # calculate some angles for the rays -> divide 180 degrees into sectors
-        others = [sympy.Point2D(data[i][testframe][0], data[i][testframe][1]) for i in range(1, len(ls))]
-
-        # calculate intensity vectors for walls (wall_view)
-        num_wall_rays = 4  # = length of wall_view
-        wall_view, intersections = wall_distances(tank, obs_pos, num_wall_rays)
-        # uncomment to plot the wall rays
-        # plot_wall_rays(tank, obs_pos, intersections)
-
-        # calculate intensity vector for nearby guppies
-        num_bins = 6  # = length of agent_view
-        agent_view = guppy_distances(tank, obs_pos, obs_angle, 6, others)
-
-    # for x in intersections:
-    #     print(x.evalf())
+    gc = Guppy_Calculator(f, agent=0, num_bins=7, num_rays=5)
+    print("Example: Handcrafted vector for frame 100:\n", gc.craft_vector(100))
+    gc.run_sim(step=5)
